f01d1f34f32633377885ec967cee3add68b02a8c
Kyle Cui <37028996+kcui5@users.noreply.github.com>
2023-11-22 16:11:14 -0800

    take args

--- pd_home.py
+++ pd_home.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pd_alt import getSumTwo
 
-def getSum():
-    x = np.array([1, 2, 3])
-    return np.sum(x) + getSumTwo()
+def getSum(x, y):
+    a = np.array([x, y, 3])
+    return np.sum(a) + getSumTwo()
