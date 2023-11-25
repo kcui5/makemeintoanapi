06d4f4973706a578b21a5f6d5cf3bba5bfbf44a4
Kyle Cui <37028996+kcui5@users.noreply.github.com>
2023-11-24 19:06:14 -0800

    tiktoken string

--- pd_home.py
+++ pd_home.py
@@ -1,6 +1,11 @@
 import numpy as np
+import tiktoken
 from pd_alt import getSumTwo
 
 def getSum(x, y):
     a = np.array([x, y, 3])
-    return np.sum(a) + getSumTwo()
+    tokenizer = tiktoken.encoding_for_model('gpt-3.5-turbo')
+    text = "Hello I am in pd modal getsum, I'd like to use tiktoken to get the length of the tokens of this text!"
+    tokens = tokenizer.encode(text)
+  
+    return np.sum(a) + getSumTwo() + len(tokens)
