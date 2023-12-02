681d7e22f845dc1a304408b1db3cf257e4a0e9cc
Kyle <kyle.tz.cui@gmail.com>
2023-12-02 14:33:13 -0800

    fixed circular import

--- pd_alt.py
+++ pd_alt.py
@@ -1,6 +1,6 @@
 import numpy as np
-from fo.pd_home import getSumThree
 
 def getSumTwo():
     x = np.array([1, 2, 3, 4, 5])
+    from fo.pd_home import getSumThree
     return np.sum(x) + getSumThree()
